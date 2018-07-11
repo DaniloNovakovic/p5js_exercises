4cf4fdb49f39552fb91d5bfb638d820bc920984a
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-11 13:02:48 +0200

    major update
    I updated bouncy ball program completely.
    Now i have class Ball and Color, and i have also array of balls. At the beginning of the program i ask user for the number of balls and then they are generated. I also added global variables for example: copyOnHit or colorsOn which you can change in dev mode by pressing F12 and see interesting result.
    My future plan is to add simple UI / Form in html so that user (non-programmer) can interact with this simulation (change settings like you can currently in dev mode)

--- bouncingBall/sketch.js
+++ bouncingBall/sketch.js
@@ -1,84 +1,130 @@
-var col = {
-    r: 255,
-    g: 255,
-    b: 255
-};
-
-var ball = {
-    x: 0,
-    y: 0,
-    diameter: 50,
-    xspeed: 4,
-    yspeed: 4,
-    moveLeft: true,
-    moveUp: true
-};
-
-var xSpeedMin = 2,
-    xSpeedMax = 7,
-    ySpeedMin = 2,
-    ySpeedMax = 7;
+let balls = [];
+let copyOnHit = false;
+let resizeCopy = false;
+let maxBalls = 200;
+let maxBallSize = 35;
+let minBallSize = 1;
+let colorsOn = false;
+let xSpeedMin = 2;
+let xSpeedMax = 7;
+let ySpeedMin = 2;
+let ySpeedMax = 7;
 
 function setup() {
-    createCanvas(400, 300);
-    ball.x = width / 2;
-    ball.y = height / 2;
+    createCanvas(600, 400);
+    let nBalls = parseInt(prompt("Enter number of balls:"), 10);
+    createBalls(nBalls);
 }
 
 function draw() {
     background(0);
-    display(ball, col);
-    bounce(ball, col);
-    move(ball);
-}
-
-function display(argBall, argCol) {
-    stroke(argCol.r, argCol.g, argCol.b);
-    strokeWeight(4);
-    noFill();
-    ellipse(argBall.x, argBall.y, argBall.diameter);
-}
-
-function bounce(argBall, argCol) {
-    // Bounce left/right
-    if (argBall.x + argBall.diameter / 2 >= width) {
-        argBall.moveLeft = true;
-        argBall.xspeed = random(xSpeedMin, xSpeedMax);
-        setRandCol(argCol);
-    } else if (ball.x - ball.diameter / 2 <= 0) {
-        argBall.moveLeft = false;
-        argBall.xspeed = random(xSpeedMin, xSpeedMax);
-        setRandCol(argCol);
-    }
-
-    // Bounce up/down
-    if (argBall.y + argBall.diameter / 2 >= height) {
-        argBall.moveUp = true;
-        argBall.yspeed = random(ySpeedMin, ySpeedMax);
-        setRandCol(argCol);
-    } else if (ball.y - ball.diameter / 2 <= 0) {
-        argBall.moveUp = false;
-        argBall.yspeed = random(ySpeedMin, ySpeedMax);
-        setRandCol(argCol);
+    let n = balls.length;
+    for (let i = 0; i < n; ++i) {
+        balls[i].show();
+        balls[i].bounce();
+        balls[i].move();
     }
 }
 
-function setRandCol(argCol) {
-    argCol.r = random(0, 255);
-    argCol.g = random(0, 255);
-    argCol.b = random(0, 255);
+function createBalls(nBalls) {
+    balls.length = 0;
+    for (let i = 0; i < nBalls; ++i) {
+        balls.push(new Ball(width / 2, height / 2, random(minBallSize, maxBallSize)));
+    }
+}
+class Color {
+    constructor(r = 255, g = 255, b = 255) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    setRandCol() {
+        if (colorsOn == false) {
+            let val = random(150, 255);
+            this.r = val;
+            this.g = val;
+            this.b = val;
+        } else {
+            this.r = random(0, 255);
+            this.g = random(0, 255);
+            this.b = random(0, 255);
+        }
+    }
 }
 
-function move(argBall) {
-    if (argBall.moveLeft) {
-        argBall.x -= argBall.xspeed;
-    } else {
-        argBall.x += argBall.xspeed;
+class Ball {
+    constructor(x = 0, y = 0, diameter = 20, col = new Color(255, 255, 255)) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.col = col;
+        this.xspeed = 4;
+        this.yspeed = 4;
+        this.setRandTransparency();
+        this.isWallHit = false;
     }
 
-    if (argBall.moveUp) {
-        argBall.y -= argBall.yspeed;
-    } else {
-        argBall.y += argBall.yspeed;
+    show() {
+        noStroke();
+        fill(this.col.r, this.col.g, this.col.b, this.transparency);
+        ellipse(this.x, this.y, this.diameter);
+    }
+    bounce() {
+        if (this.y + this.diameter / 2 >= height || this.y - this.diameter / 2 <= 0 ||
+            this.x + this.diameter / 2 >= width || this.x - this.diameter / 2 <= 0) {
+
+            // Update current ball direction according to the which wall it hit
+            if (this.y + this.diameter / 2 >= height) {
+                this.yspeed = Math.abs(random(ySpeedMin, ySpeedMax)) * -1;
+            } else if (this.y - this.diameter / 2 <= 0) {
+                this.yspeed = Math.abs(random(ySpeedMin, ySpeedMax));
+            }
+
+            if (this.x + this.diameter / 2 >= width) {
+                this.xspeed = Math.abs(random(xSpeedMin, xSpeedMax)) * -1;
+            } else if (this.x - this.diameter / 2 <= 0) {
+                this.xspeed = Math.abs(random(xSpeedMin, xSpeedMax));
+            }
+
+            if (this.isWallHit == false) {
+                this.isWallHit = true;
+
+                // Make a new copy of the ball and move it away from the edge
+                if (copyOnHit && balls.length < maxBalls) {
+                    let currBall = this.copy();
+                    currBall.col.setRandCol();
+                    let xval = this.xspeed > 0 ? 1 : -1;
+                    let yval = this.yspeed > 0 ? 1 : -1;
+                    currBall.x += currBall.diameter / 2 * xval;
+                    currBall.y += currBall.diameter / 2 * yval;
+                    balls.push(currBall);
+                }
+                this.col.setRandCol();
+                this.setRandTransparency();
+            }
+        } else {
+            this.isWallHit = false;
+        }
+    }
+    move() {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+    setRandTransparency() {
+        this.transparency = random(20, 255);
+    }
+    copy() {
+        let col = new Color(this.col.r, this.col.g, this.col.b);
+        let value = resizeCopy == true ? random(minBallSize, maxBallSize) : this.diameter;
+        let retBall = new Ball(this.x, this.y, value, col, this.moveLeft, this.moveUp);
+        retBall.xspeed = this.xspeed;
+        retBall.yspeed = this.yspeed;
+        retBall.xSpeedM = this.xSpeedM;
+        retBall.xSpeedM = this.xSpeedM;
+        retBall.ySpeedM = this.ySpeedM;
+        retBall.ySpeedM = this.ySpeedM;
+        retBall.isWallHit = this.isWallHit;
+        retBall.setRandTransparency();
+        return retBall;
     }
 }
