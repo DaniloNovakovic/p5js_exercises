9d676e0794373d7d9399664a366d93da67afd041
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-15 19:20:22 +0200

    added option to turn on/off flashing colors

--- minSpanningTree/sketch.js
+++ minSpanningTree/sketch.js
@@ -3,27 +3,37 @@ var tree = {
     edges: []
 };
 var resetBtn;
+var flashColBtn;
+var flashingColors = false;
+var interval;
 
 function setup() {
     createCanvas(600, 400);
     background(0);
+    changeColor();
+
     createP('');
     resetBtn = createButton('Reset');
-    resetBtn.mousePressed(resetBtnClicked);
-    setInterval(changeColor, 1000);
+    resetBtn.mousePressed(resetBtnPressed);
+
+    flashColBtn = createButton('Turn On Flashing Colors');
+    flashColBtn.style('marginLeft', '10px');
+    flashColBtn.mousePressed(flashColBtnPressed);
 }
 
-function draw() {
-    background(0);
-    drawTree(tree);
+function flashColBtnPressed() {
+    flashingColors = !flashingColors;
+    if (flashingColors) {
+        interval = setInterval(changeColor, 2000);
+        this.html('Turn Off Flashing Colors');
+    } else {
+        clearInterval(interval);
+        this.html('Turn On Flashing Colors');
+    }
+    changeColor();
 }
 
-function changeColor() {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    stroke(random(0, 255), random(0, 255), random(0, 255));
-}
-
-function resetBtnClicked() {
+function resetBtnPressed() {
     background(0);
     tree.vertices.length = 0;
     tree.edges.length = 0;
@@ -35,6 +45,21 @@ function mousePressed() {
         tree.vertices.push(v);
         tree.edges = connectVertices(tree.vertices);
     }
+}
+
+function changeColor() {
+    if (flashingColors) {
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        stroke(random(0, 255), random(0, 255), random(0, 255));
+    } else {
+        fill(255);
+        stroke(255);
+    }
+}
+
+function draw() {
+    background(0);
+    drawTree(tree);
 }
 
 function drawTree(argTree) {
