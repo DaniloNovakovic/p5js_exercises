c66878b454902ee61f536ab79014d0218052675e
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-17 14:54:53 +0200

    added ability to enter with 'ENTER'
    -  you can submit your name by pressing ENTER aswell as pressing the submit btn

--- gifFinder/sketch.js
+++ gifFinder/sketch.js
@@ -1,34 +1,43 @@
 var api = "https://api.giphy.com/v1/gifs/search?q=";
-var name = "";
+var name = '';
 var api_key = '&api_key=5bqp72P9orXVJcOyiRvXsdKTzFdql5il';
 var limit = "&limit=5";
 var inputBox;
-var submitBtn;
+var btnSubmit;
 var images = [];
 
 function setup() {
     noCanvas();
     inputBox = select('#giphySearchBox');
-    submitBtn = select('#submitBtn');
-    submitBtn.mousePressed(askGiphy);
+    btnSubmit = select('#submitBtn');
+    btnSubmit.mousePressed(askGiphy);
+}
+
+function keyPressed() {
+    if (keyCode == ENTER) {
+        askGiphy();
+    }
 }
 
 function askGiphy() {
-    var url = api + inputBox.value() + api_key;
-    loadJSON(url, gotData);
+    if (name != inputBox.value()) {
+        name = inputBox.value();
+        var url = api + name + api_key;
+        loadJSON(url, gotData);
+    }
 }
 
 function gotData(giphy) {
+    console.log(name);
     eraseImages();
-    for(let val of giphy.data) {
-        console.log(val.images.original.url);
+    for (let val of giphy.data) {
         images.push(createImg(val.images.original.url));
     }
 }
+
 function eraseImages() {
-    for(var img of images) {
+    for (var img of images) {
         img.remove();
     }
     images.length = 0;
 }
-
