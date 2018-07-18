fecbd438aa128c47c579624bb29c16eb34e15de0
DaniloNovakovic <dakenzi97@gmail.com>
2018-07-18 10:08:50 +0200

    added camera status messages
    - Because some of the people i sent this project have had either their camera disconnected withouth them knowing or some other issue i added try/catch everywhere with console..log and appropriate status message on paragraph above. Also added camera loading to preload. Hopefully this will fix all the errors / confusion

--- domInputBoxCamera/sketch.js
+++ domInputBoxCamera/sketch.js
@@ -7,19 +7,29 @@ var cols = 40;
 var rows = 30;
 
 var boxes = [];
+var pleaseConnectCameraP;
+var cameraConnected = false;
 
+function preload() {
+    pleaseConnectCameraP = select('#connectCameraMsg');
+    try {
+        video = createCapture(VIDEO, gotVideo);
+        video.size(cols, rows);
+        video.hide();
+    } catch(err) {
+        console.log(err.message);
+        pleaseConnectCameraP.html(err.message);
+    }
+}
 function setup() {
     noCanvas();
     pixelDensity(1);
-    video = createCapture(VIDEO);
-    video.size(cols, rows);
-    video.hide();
 
     for (var y = 0; y < rows; ++y) {
         for (var x = 0; x < cols; ++x) {
             var box = createInput('▉'); // unicode block char U+2588
             box.parent('#mirror');
-            box.style('width', '10pt');
+            box.style('width', '15px');
             box.style('background-color', 'black');
             box.style('border', '1px  black');
             boxes.push(box);
@@ -27,20 +37,45 @@ function setup() {
         var linebreak = createSpan('<br>');
         linebreak.parent('#mirror');
     }
-
 }
-function draw() {
-    video.loadPixels();
-    for (let y = 0; y < video.height; ++y) {
-        for (let x = 0; x < video.width; ++x) {
-            let index = (video.width - x + 1 + y * video.width) * 4;
-            let r = video.pixels[index + 0];
-            let g = video.pixels[index + 1];
-            let b = video.pixels[index + 2];
+function gotVideo(data) {
+    if(data) {
+        try {
+            console.log(data.active);
+            cameraConnected = true;
+            pleaseConnectCameraP.html('Camera connected!');
+            createP('This camera is made using array of DOM input elements filled with unicode block char U+2588 ');
+        } catch(err) {
+            console.log(err.message);
+        }
+    }
+}
 
-            var ci = x + y * cols;
-            
-            boxes[ci].style('color', color(r,g,b));
+function draw() {
+    if(video) {
+        try {
+            video.loadPixels();
+            for (let y = 0; y < video.height; ++y) {
+                for (let x = 0; x < video.width; ++x) {
+                    let index = (video.width - x + 1 + y * video.width) * 4;
+                    let r = video.pixels[index + 0];
+                    let g = video.pixels[index + 1];
+                    let b = video.pixels[index + 2];
+
+                    var ci = x + y * cols;
+
+                    boxes[ci].style('color',`rgb(${r}, ${g}, ${b})`);
+                }
+            }
+        } catch(err) {
+            console.log(err.message);
+            cameraConnected = false;
+            video = null;
+        }
+    } else {
+        if(cameraConnected && pleaseConnectCameraP) {
+            pleaseConnectCameraP.html('Camera disconnected, please connect your camera and refresh page...');
+            cameraConnected = false;
         }
     }
 
